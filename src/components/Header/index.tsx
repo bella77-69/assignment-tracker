c14766a4a4d42742041127ae559d27a7a0b25f5b
chantelle mckenzie <mrspasceri@hotmail.com>
2023-10-13 13:52:54 -0700

    finished step one, button disabled when no assignment has been entered into the input

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
 
 export function Header() {
+  const [input, setInput] = useState<string>("");
+
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
       <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        {/* button should be disabled when no assignment has been entered into the input */ }
-        <button disabled>
+        <input 
+        placeholder="Add a new assignment" 
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+        <button disabled={!input}>
           <AiOutlinePlusCircle size={20} />
         </button>
       </form>
